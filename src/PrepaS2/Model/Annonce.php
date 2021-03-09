b0d40e863474232aff5e1f06bdfb2e9434272397
Xin4s <70570587+Xin4s@users.noreply.github.com>
2021-03-09 21:29:38 +0100

    debug

--- src/PrepaS2/Model/Annonce.php
+++ src/PrepaS2/Model/Annonce.php
@@ -7,6 +7,9 @@ use wishlist\PrepaS2\Model\Photo as Photo;
 use wishlist\PrepaS2\Model\Categorie as Categorie;
 class Annonce extends Model
 {
+    protected $table = 'Annonces';
+	protected $primaryKey = 'idAn';
+	public $timestamps = false;
     /**
      * Get the photo for the Annonce.
      */
--- src/PrepaS2/Model/Photo.php
+++ src/PrepaS2/Model/Photo.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 use wishlist\PrepaS2\Model\Annonce as Annonce;
 class Photo extends Model
 {
+    protected $table = 'photo';
+	protected $primaryKey = 'idPhoto';
+	public $timestamps = false;
     /**
      * Get the Annonce that owns the photo.
      */
